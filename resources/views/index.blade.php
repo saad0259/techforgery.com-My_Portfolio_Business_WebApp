c92a7095a8b907e6f68fac7feb235bec78b7bbeb
Saad Shafiq <saadshafiq259@gmail.com>
2021-04-25 14:32:19 +0500

    added design scaffold

---
+++ resources/views/index.blade.php
@@ -0,0 +1,387 @@
+@extends('../layouts.app')
+
+@section('content')
+
+  
+<section class="home-slider owl-carousel">
+    <a href="portfolio.html" class="slider-item" style="background-image: url(images/bg_1.jpg);">
+      <div class="overlay"></div>
+      <div class="container">
+        <div class="row slider-text align-items-end justify-content-center">
+          <div class="col-md-12 ftco-animate p-4" data-scrollax=" properties: { translateY: '70%' }">
+          <h1 class="mb-3" data-scrollax="properties: { translateY: '30%', opacity: 1.6 }">#01 Exterior Building</h1>
+          </div>
+        </div>
+      </div>
+    </a>
+    <a href="portfolio.html" class="slider-item" style="background-image: url('images/bg_2.jpg');">
+      <div class="overlay"></div>
+      <div class="container">
+        <div class="row slider-text align-items-end justify-content-center">
+          <div class="col-md-12 ftco-animate p-4" data-scrollax=" properties: { translateY: '70%' }">
+          <h1 class="mb-3" data-scrollax="properties: {translateY: '30%', opacity: 1.6}">#02 American Building</h1>
+          </div>
+        </div>
+      </div>
+    </a>
+    <a href="portfolio.html" class="slider-item" style="background-image: url('images/bg_3.jpg');">
+      <div class="overlay"></div>
+      <div class="container">
+        <div class="row slider-text align-items-end justify-content-center">
+          <div class="col-md-12 ftco-animate p-4" data-scrollax=" properties: { translateY: '70%' }">
+          <h1 class="mb-3" data-scrollax="properties: {translateY: '30%', opacity: 1.6}">#03 Louie's Residences</h1>
+          </div>
+        </div>
+      </div>
+    </a>
+    <a href="portfolio.html" class="slider-item" style="background-image: url('images/bg_4.jpg');">
+      <div class="overlay"></div>
+      <div class="container">
+        <div class="row slider-text align-items-end justify-content-center">
+          <div class="col-md-12 ftco-animate p-4" data-scrollax=" properties: { translateY: '70%' }">
+          <h1 class="mb-3" data-scrollax="properties: {translateY: '30%', opacity: 1.6}">#04 Business Building</h1>
+          </div>
+        </div>
+      </div>
+    </a>
+  </section>
+
+  <section class="ftco-counter" id="section-counter">
+    <div class="container">
+      <div class="row d-flex">
+        <div class="col-md-7 py-sm-3 py-md-5">
+          <div class="row py-5">
+            <div class="col-md-6 justify-content-center counter-wrap ftco-animate">
+              <div class="block-18">
+                <div class="text">
+                  <strong class="number" data-number="120">0</strong>
+                  <span>Finished Projects</span>
+                </div>
+              </div>
+            </div>
+            <div class="col-md-6 justify-content-center counter-wrap ftco-animate">
+              <div class="block-18">
+                <div class="text">
+                  <strong class="number" data-number="112">0</strong>
+                  <span>happy Customers</span>
+                </div>
+              </div>
+            </div>
+            <div class="col-md-6 justify-content-center counter-wrap ftco-animate">
+              <div class="block-18">
+                <div class="text">
+                  <strong class="number" data-number="700">0</strong>
+                  <span>Working Hours</span>
+                </div>
+              </div>
+            </div>
+            <div class="col-md-6 justify-content-center counter-wrap ftco-animate">
+              <div class="block-18">
+                <div class="text">
+                  <strong class="number" data-number="850">0</strong>
+                  <span>Cups of coffee</span>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-5 d-flex justify-content-center align-items-center img exp" style="background-image: url(images/image_1.jpg);">
+          <p><a href="#" class="btn btn-white px-5 py-4">Explore Further</a></p>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <section class="ftco-services">
+    <div class="container">
+      <div class="row no-gutters">
+        <div class="col-md-5 ftco-animate py-5 nav-link-wrap aside-stretch">
+          <div class="nav flex-column nav-pills" id="v-pills-tab" role="tablist" aria-orientation="vertical">
+            <h3 class="ml-3">Services</h3>
+            <a class="nav-link px-4 active" id="v-pills-master-tab" data-toggle="pill" href="#v-pills-master" role="tab" aria-controls="v-pills-master" aria-selected="true"><span class="mr-3 flaticon-house-plans"></span> Interior</a>
+            <a class="nav-link px-4" id="v-pills-buffet-tab" data-toggle="pill" href="#v-pills-buffet" role="tab" aria-controls="v-pills-buffet" aria-selected="false"><span class="mr-3 flaticon-ideas"></span> Concept</a>
+            <a class="nav-link px-4" id="v-pills-fitness-tab" data-toggle="pill" href="#v-pills-fitness" role="tab" aria-controls="v-pills-fitness" aria-selected="false"><span class="mr-3 flaticon-modern-house"></span> Residential</a>
+            <a class="nav-link px-4" id="v-pills-reception-tab" data-toggle="pill" href="#v-pills-reception" role="tab" aria-controls="v-pills-reception" aria-selected="false"><span class="mr-3 flaticon-skyline"></span> Hospitality</a>
+          </div>
+        </div>
+        <div class="col-md-7 ftco-animate p-4 p-md-5 d-flex align-items-center">
+          <div class="tab-content pl-md-5" id="v-pills-tabContent">
+            <div class="tab-pane fade show active py-5" id="v-pills-master" role="tabpanel" aria-labelledby="v-pills-master-tab">
+              <span class="icon mb-3 d-block flaticon-house-plans"></span>
+              <h2 class="mb-4">Interior Design</h2>
+              <p>Inventore fugit error iure nisi reiciendis fugiat illo pariatur quam sequi quod iusto facilis officiis nobis sit quis molestias asperiores rem, blanditiis! Commodi exercitationem vitae deserunt qui nihil ea, tempore et quam natus quaerat doloremque.</p>
+              <p><a href="#" class="btn btn-primary px-4 py-3">Learn More</a></p>
+            </div>
+            <div class="tab-pane fade py-5" id="v-pills-buffet" role="tabpanel" aria-labelledby="v-pills-buffet-tab">
+              <span class="icon mb-3 d-block flaticon-ideas"></span>
+              <h2 class="mb-4">Concept Design</h2>
+              <p>Inventore fugit error iure nisi reiciendis fugiat illo pariatur quam sequi quod iusto facilis officiis nobis sit quis molestias asperiores rem, blanditiis! Commodi exercitationem vitae deserunt qui nihil ea, tempore et quam natus quaerat doloremque.</p>
+              <p><a href="#" class="btn btn-primary px-4 py-3">Learn More</a></p>
+            </div>
+            <div class="tab-pane fade py-5" id="v-pills-fitness" role="tabpanel" aria-labelledby="v-pills-fitness-tab">
+              <span class="icon mb-3 d-block flaticon-modern-house"></span>
+              <h2 class="mb-4">Residential Design</h2>
+              <p>Inventore fugit error iure nisi reiciendis fugiat illo pariatur quam sequi quod iusto facilis officiis nobis sit quis molestias asperiores rem, blanditiis! Commodi exercitationem vitae deserunt qui nihil ea, tempore et quam natus quaerat doloremque.</p>
+              <p><a href="#" class="btn btn-primary px-4 py-3">Learn More</a></p>
+            </div>
+            <div class="tab-pane fade py-5" id="v-pills-reception" role="tabpanel" aria-labelledby="v-pills-reception-tab">
+              <span class="icon mb-3 d-block flaticon-skyline"></span>
+              <h2 class="mb-4">Hospitality Design</h2>
+              <p>Inventore fugit error iure nisi reiciendis fugiat illo pariatur quam sequi quod iusto facilis officiis nobis sit quis molestias asperiores rem, blanditiis! Commodi exercitationem vitae deserunt qui nihil ea, tempore et quam natus quaerat doloremque.</p>
+              <p><a href="#" class="btn btn-primary px-4 py-3">Learn More</a></p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <section class="ftco-quote ftco-animate">
+    <div class="container">
+      <div class="row d-flex justify-content-end">
+        <div class="col-md-7 req-quote d-md-flex py-5 align-items-center img" style="background-image: url(images/bg_1.jpg);">
+          <h3 class=" ml-md-3">Get started</h3>
+          <p><a href="#" class="btn btn-white py-4 px-4" data-toggle="modal" data-target="#modalRequest">Request a quote</a></p>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <section class="ftco-section">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-4">
+            <div class="row">
+              <div class="col-md-12 heading-section ftco-animate mb-3">
+                <h2 class="mb-4">Portfolio</h2>
+                <p>Far far away, behind the word mountains, far from the countries Vokalia </p>
+              </div>
+            </div>
+          <a href="portfolio.html" class="portfolio ftco-animate">
+            <div class="d-flex icon justify-content-center align-items-center">
+              <span class="ion-md-search"></span>
+            </div>
+            <div class="d-flex heading align-items-end">
+              <h3>
+                <span>Residence</span><br>
+                Work 01
+              </h3>
+            </div>
+            <img src="images/image_3.jpg" class="img-fluid" alt="techforgery Template">
+          </a>
+        <a href="portfolio.html" class="portfolio ftco-animate">
+          <div class="d-flex icon justify-content-center align-items-center">
+            <span class="ion-md-search"></span>
+          </div>
+          <div class="d-flex heading align-items-end">
+            <h3>
+              <span>Residence</span><br>
+              Work 02
+            </h3>
+          </div>
+          <img src="images/image_2.jpg" class="img-fluid" alt="techforgery Template">
+        </a>
+        <a href="portfolio.html" class="portfolio ftco-animate">
+          <div class="d-flex icon justify-content-center align-items-center">
+            <span class="ion-md-search"></span>
+          </div>
+          <div class="d-flex heading align-items-end">
+            <h3>
+              <span>Residence</span><br>
+              Work 03
+            </h3>
+          </div>
+          <img src="images/image_9.jpg" class="img-fluid" alt="techforgery Template">
+        </a>
+      </div>
+
+      <div class="col-md-8">
+          <a href="portfolio.html" class="portfolio ftco-animate">
+            <div class="d-flex icon justify-content-center align-items-center">
+              <span class="ion-md-search"></span>
+            </div>
+            <div class="d-flex heading align-items-end">
+              <h3>
+                <span>Residence</span><br>
+                Work 04
+              </h3>
+            </div>
+            <img src="images/image_1.jpg" class="img-fluid" alt="techforgery Template">
+          </a>
+        <div class="row">
+          <div class="col-md-6">
+            <a href="portfolio.html" class="portfolio ftco-animate">
+              <div class="d-flex icon justify-content-center align-items-center">
+                <span class="ion-md-search"></span>
+              </div>
+              <div class="d-flex heading align-items-end">
+                <h3>
+                  <span>Residence</span><br>
+                  Work 05
+                </h3>
+              </div>
+              <img src="images/image_5.jpg" class="img-fluid" alt="techforgery Template">
+            </a>
+          </div>
+          <div class="col-md-6">
+            <a href="portfolio.html" class="portfolio ftco-animate">
+              <div class="d-flex icon justify-content-center align-items-center">
+                <span class="ion-md-search"></span>
+              </div>
+              <div class="d-flex heading align-items-end">
+                <h3>
+                  <span>Residence</span><br>
+                  Work 06
+                </h3>
+              </div>
+              <img src="images/image_6.jpg" class="img-fluid" alt="techforgery Template">
+            </a>
+          </div>
+        </div>
+
+
+        <div class="row">
+        <div class="col-md-8">
+        <a href="portfolio.html" class="portfolio ftco-animate">
+        <div class="d-flex icon justify-content-center align-items-center">
+        <span class="ion-md-search"></span>
+        </div>
+        <div class="d-flex heading align-items-end">
+        <h3>
+        <span>Residence</span><br>
+        Work 07
+        </h3>
+        </div>
+        <img src="images/image_7.jpg" class="img-fluid" alt="techforgery Template">
+        </a>
+        </div>
+        <div class="col-md-4">
+        <a href="portfolio.html" class="portfolio ftco-animate">
+        <div class="d-flex icon justify-content-center align-items-center">
+        <span class="ion-md-search"></span>
+        </div>
+        <div class="d-flex heading align-items-end">
+        <h3>
+        <span>Residence</span><br>
+        Work 08
+        </h3>
+        </div>
+        <img src="images/image_4.jpg" class="img-fluid" alt="techforgery Template">
+        </a>
+        </div>
+        </div>
+        </div>
+      </div>
+    </div>
+  </section>
+
+    <section class="testimony-section">
+        <div class="container">
+        <div class="row d-md-flex">
+        <div class="col-md-4 last-order d-md-flex align-items-start heading-section aside-stretch ftco-animate">
+        <div>
+        <h2 class="mb-4">Clients Says</h2>
+        <p>Far far away, behind the word mountains, far from the countries Vokalia </p>
+        </div>
+        </div>
+        <div class="col-md-8 first-order ftco-animate">
+        <div class="carousel-testimony owl-carousel">
+        <div class="item">
+        <div class="testimony-wrap text-center">
+        <div class="user-img mb-5" style="background-image: url(images/person_1.jpg)">
+        <span class="quote d-flex align-items-center justify-content-center">
+        <i class="icon-quote-left"></i>
+        </span>
+        </div>
+        <div class="text">
+        <p class="mb-5">Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. Separated they live in Bookmarksgrove right at the coast of the Semantics, a large language ocean.</p>
+        <p class="name">Dennis Green</p>
+        <span class="position">Architect</span>
+        </div>
+        </div>
+        </div>
+        <div class="item">
+        <div class="testimony-wrap text-center">
+        <div class="user-img mb-5" style="background-image: url(images/person_2.jpg)">
+        <span class="quote d-flex align-items-center justify-content-center">
+        <i class="icon-quote-left"></i>
+        </span>
+        </div>
+        <div class="text">
+        <p class="mb-5">Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. Separated they live in Bookmarksgrove right at the coast of the Semantics, a large language ocean.</p>
+        <p class="name">Dennis Green</p>
+        <span class="position">Architect</span>
+        </div>
+        </div>
+        </div>
+        <div class="item">
+        <div class="testimony-wrap text-center">
+        <div class="user-img mb-5" style="background-image: url(images/person_3.jpg)">
+        <span class="quote d-flex align-items-center justify-content-center">
+        <i class="icon-quote-left"></i>
+        </span>
+        </div>
+        <div class="text">
+        <p class="mb-5">Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. Separated they live in Bookmarksgrove right at the coast of the Semantics, a large language ocean.</p>
+        <p class="name">Dennis Green</p>
+        <span class="position">Architect</span>
+        </div>
+        </div>
+        </div>
+        <div class="item">
+        <div class="testimony-wrap text-center">
+        <div class="user-img mb-5" style="background-image: url(images/person_3.jpg)">
+        <span class="quote d-flex align-items-center justify-content-center">
+        <i class="icon-quote-left"></i>
+        </span>
+        </div>
+        <div class="text">
+        <p class="mb-5">Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. Separated they live in Bookmarksgrove right at the coast of the Semantics, a large language ocean.</p>
+        <p class="name">Dennis Green</p>
+        <span class="position">Customer</span>
+        </div>
+        </div>
+        </div>
+        </div>
+        </div>
+        </div>
+        </div>
+    </section>
+    <section class="ftco-section bg-light">
+        <div class="container">
+        <div class="row">
+        <div class="col-md-4 mb-3 text-center ftco-animate">
+        <div class="pricing p-4 py-5">
+        <div class="icon mb-2"><span class="flaticon-skyline"></span></div>
+        <h3 class="mb-3">Basic Plan</h3>
+        <p class="mb-4">Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.</p>
+        <p class="price mb-4 py-3"><span class="mr-2 currency">$29</span> <span class="per">per month</span></p>
+        <p><a href="#" class="btn btn-primary px-4 py-3">Get started</a></p>
+        <a href="#">Open Source</a>
+        </div>
+        </div>
+        <div class="col-md-4 mb-3 text-center ftco-animate">
+        <div class="pricing p-4 py-5">
+        <div class="icon mb-2"><span class="flaticon-skyline"></span></div>
+        <h3 class="mb-3">Standard Plan</h3>
+        <p class="mb-4">Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.</p>
+        <p class="price mb-4 py-3"><span class="mr-2 currency">$29</span> <span class="per">per month</span></p>
+        <p><a href="#" class="btn btn-primary px-4 py-3">Get started</a></p>
+        <a href="#">Free 30 Day Trial</a>
+        </div>
+        </div>
+        <div class="col-md-4 mb-3 text-center ftco-animate">
+        <div class="pricing p-4 py-5">
+        <div class="icon mb-2"><span class="flaticon-skyline"></span></div>
+        <h3 class="mb-3">Premium Plan</h3>
+        <p class="mb-4">Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.</p>
+        <p class="price mb-4 py-3"><span class="mr-2 currency">$29</span> <span class="per">per month</span></p>
+        <p><a href="#" class="btn btn-primary px-4 py-3">Get started</a></p>
+        <a href="#" data-toggle="modal" data-target="#modalRequest">Request a Quote</a>
+        </div>
+        </div>
+        </div>
+        </div>
+    </section>
+
+@endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,36 +1,168 @@
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta name="csrf-token" content="{{ csrf_token() }}">
+<html lang="en">
 
-        <title>{{ config('app.name', 'Laravel') }}</title>
+<head>
+<title>Tech Forgery</title>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+<link href="https://fonts.googleapis.com/css?family=Rubik:300,400,500,700" rel="stylesheet">
+<link rel="stylesheet" href="css/open-iconic-bootstrap.min.css">
+<link rel="stylesheet" href="css/animate.css">
+<link rel="stylesheet" href="css/owl.carousel.min.css">
+<link rel="stylesheet" href="css/owl.theme.default.min.css">
+<link rel="stylesheet" href="css/magnific-popup.css">
+<link rel="stylesheet" href="css/aos.css">
+<link rel="stylesheet" href="css/ionicons.min.css">
+<link rel="stylesheet" href="css/bootstrap-datepicker.css">
+<link rel="stylesheet" href="css/jquery.timepicker.css">
+<link rel="stylesheet" href="css/flaticon.css">
+<link rel="stylesheet" href="css/icomoon.css">
+<link rel="stylesheet" href="css/style.css">
+</head>
+<body>
 
-        <!-- Fonts -->
-        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
-
-        <!-- Styles -->
-        <link rel="stylesheet" href="{{ asset('css/app.css') }}">
-
-        <!-- Scripts -->
-        <script src="{{ asset('js/app.js') }}" defer></script>
-    </head>
-    <body class="font-sans antialiased">
-        <div class="min-h-screen bg-gray-100">
-            @include('layouts.navigation')
-
-            <!-- Page Heading -->
-            <header class="bg-white shadow">
-                <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-                    {{ $header }}
+<div class="page">
+    <nav id="techforgery-main-nav"  class="border" role="navigation">
+        <a href="#" class="js-techforgery-nav-toggle techforgery-nav-toggle active "><i></i></a>
+        <div class="js-fullheight techforgery-table">
+        <div class="img" style="background-image: url(images/bg_2.jpg);"></div>
+        <div class="techforgery-table-cell js-fullheight">
+            <div class="row no-gutters">
+                <div class="col-md-12 text-center">
+                <h1 class="mb-4"><a href="index.php" class="logo">Tech Forgery</a></h1>
+                    <ul>
+                    <li class="active"><a href="index.php"><span><small>01</small>Home</span></a></li>
+                    <li><a href="about.html"><span><small>02</small>About</span></a></li>
+                    <li><a href="services.html"><span><small>03</small>Services</span></a></li>
+                    <li><a href="pricing.html"><span><small>04</small>Pricing</span></a></li>
+                    <li><a href="portfolio.html"><span><small>05</small>Portfolio</span></a></li>
+                    <li><a href="blog.html"><span><small>06</small>Blog</span></a></li>
+                    <li><a href="contact.html"><span><small>07</small>Contact</span></a></li>
+                    </ul>
                 </div>
-            </header>
-
-            <!-- Page Content -->
-            <main>
-                {{ $slot }}
-            </main>
+            </div>
         </div>
-    </body>
+        </div>
+    </nav>
+
+
+  <div id="techforgery-page">
+ 
+    <header>
+      <div class="techforgery-navbar-brand text-center">
+      <a class="techforgery-logo" href="https://techforgery.com/">Tech Forgery</a>
+      </div>
+      <a href="#" class="js-techforgery-nav-toggle techforgery-nav-toggle"><i></i></a>
+    </header>
+    
+    @yield('content')
+  
+
+
+
+
+<footer class="ftco-footer ftco-bg-dark ftco-section">
+    <div class="container">
+    <div class="row mb-5 justify-content-center">
+    <div class="col-md-5 text-center">
+    <div class="ftco-footer-widget mb-3">
+    <ul class="ftco-footer-social list-unstyled">
+    <li class="ftco-animate"><a href="#"><span class="icon-twitter"></span></a></li>
+    <li class="ftco-animate"><a href="#"><span class="icon-facebook"></span></a></li>
+    <li class="ftco-animate"><a href="#"><span class="icon-instagram"></span></a></li>
+    </ul>
+    </div>
+    <div class="ftco-footer-widget">
+    <h2 class="mb-3">Contact Us</h2>
+    <p class="email"><a href="#"><span class="__cf_email__" data-cfemail="d9b0b7bfb699bcb4b8b0b5f7bab6b4">[email&#160;protected]</span></a></p>
+    </div>
+    </div>
+    </div>
+    <div class="row">
+    <div class="col-md-12 text-center">
+    <p>
+    Copyright &copy;<script data-cfasync="false" src="../../cdn-cgi/scripts/5c5dd728/cloudflare-static/email-decode.min.js"></script><script>document.write(new Date().getFullYear());</script> All rights reserved | This template is made with <i class="icon-heart" aria-hidden="true" style="color: red;"></i> by <a href="https://techforgery.com/" target="_blank">techforgery</a>
+    </p>
+    </div>
+    </div>
+    </div>
+</footer>
+
+<div id="ftco-loader" class="show fullscreen"><svg class="circular" width="48px" height="48px"><circle class="path-bg" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke="#eeeeee" /><circle class="path" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke-miterlimit="10" stroke="#F96D00" /></svg></div>
+</div>
+
+<div class="modal fade" id="modalRequest" tabindex="-1" role="dialog" aria-labelledby="modalRequestLabel" aria-hidden="true">
+<div class="modal-dialog" role="document">
+<div class="modal-content">
+<div class="modal-header">
+<h5 class="modal-title" id="modalRequestLabel">Request a Quote</h5>
+<button type="button" class="close" data-dismiss="modal" aria-label="Close">
+<span aria-hidden="true">&times;</span>
+</button>
+</div>
+<div class="modal-body">
+<form action="#">
+<div class="form-group">
+<label for="appointment_name" class="text-black">Full Name</label>
+<input type="text" class="form-control" id="appointment_name">
+</div>
+<div class="form-group">
+<label for="appointment_email" class="text-black">Email</label>
+<input type="text" class="form-control" id="appointment_email">
+</div>
+<div class="row">
+<div class="col-md-6">
+<div class="form-group">
+<label for="appointment_date" class="text-black">Date</label>
+<input type="text" class="form-control" id="appointment_date">
+</div>
+</div>
+<div class="col-md-6">
+<div class="form-group">
+<label for="appointment_time" class="text-black">Time</label>
+<input type="text" class="form-control" id="appointment_time">
+</div>
+</div>
+</div>
+<div class="form-group">
+<label for="appointment_message" class="text-black">Message</label>
+<textarea name="" id="appointment_message" class="form-control" cols="30" rows="10"></textarea>
+</div>
+<div class="form-group">
+<input type="submit" value="Send Message" class="btn btn-primary">
+</div>
+</form>
+</div>
+</div>
+</div>
+</div>
+</div>
+<script src="js/jquery.min.js"></script>
+<script src="js/jquery-migrate-3.0.1.min.js"></script>
+<script src="js/popper.min.js"></script>
+<script src="js/bootstrap.min.js"></script>
+<script src="js/jquery.easing.1.3.js"></script>
+<script src="js/jquery.waypoints.min.js"></script>
+<script src="js/jquery.stellar.min.js"></script>
+<script src="js/owl.carousel.min.js"></script>
+<script src="js/jquery.magnific-popup.min.js"></script>
+<script src="js/aos.js"></script>
+<script src="js/jquery.animateNumber.min.js"></script>
+<script src="js/scrollax.min.js"></script>
+<script src="js/bootstrap-datepicker.js"></script>
+<script src="js/jquery.timepicker.min.js"></script>
+<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBVWaKrjvy3MaE7SQ74_uJiULgl1JY0H2s&amp;sensor=false"></script>
+<script src="js/google-map.js"></script>
+<script src="js/main.js"></script>
+
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-23581568-13"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-23581568-13');
+</script>
+</body>
+
 </html>
